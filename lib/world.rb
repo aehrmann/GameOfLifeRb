bd664ddb06a5d0f92f12eec7d3382b55e51d364f
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-06-09 14:27:18 -0500

    Add World#count_living_neighbors

--- lib/world.rb
+++ lib/world.rb
@@ -3,11 +3,36 @@ class World
     @cells = Array.new(dimension) { Array.new(dimension, false) }
   end
 
-  def living_cell?(x, y)
-    @cells[y][x]
+  def living_cell?(row, column)
+    @cells[row][column]
   end
 
-  def spawn_cell_at(x, y)
-    @cells[y][x] = true
+  def spawn_cell_at(row, column)
+    @cells[row][column] = true
+  end
+
+  def count_living_neighbors(row, col)
+    all_neighbor_coordinates(row, col).reduce(0) do |count, (row, col)|
+      count += 1 if living_cell?(row, col)
+      count
+    end
+  end
+
+  def all_neighbor_coordinates(row, col)
+    top_neighbor_coordinates(row, col) +
+    side_neighbor_coordinates(row, col) +
+    bottom_neighbor_coordinates(row, col)
+  end
+
+  def top_neighbor_coordinates(row, col)
+    [[row - 1, col - 1], [row - 1, col], [row - 1, col + 1]]
+  end
+
+  def side_neighbor_coordinates(row, col)
+    [[row, col - 1], [row, col + 1]]
+  end
+
+  def bottom_neighbor_coordinates(row, col)
+    [[row + 1, col - 1], [row + 1, col], [row + 1, col + 1]]
   end
 end
--- spec/lib/world_spec.rb
+++ spec/lib/world_spec.rb
@@ -2,15 +2,43 @@ require 'world'
 
 describe World do
   describe "checking a cell's living status" do
+    before(:each) { @world = World.new(10) }
+
     it "returns false if there is no cell at the given coordinates" do
-      world = World.new(10)
-      expect(world.living_cell?(2, 2)).to be false
+      expect(@world.living_cell?(2, 8)).to be false
     end
 
     it "returns true if there a cell at the given position" do
-      world = World.new(10)
-      world.spawn_cell_at(5, 5)
-      expect(world.living_cell?(5, 5)).to be true
+      @world.spawn_cell_at(5, 2)
+      expect(@world.living_cell?(5, 2)).to be true
+    end
+  end
+
+  describe "retrieving the number of living neighbors for a cell" do
+    before(:each) { @world = World.new(10) }
+
+    it "returns 0 for a cell with no living neighbors" do
+      expect(@world.count_living_neighbors(1, 9)).to eq(0)
+    end
+
+    it "returns the number of living neighbors in the row above the given cell" do
+      @world.spawn_cell_at(3, 3)
+      @world.spawn_cell_at(3, 4)
+      @world.spawn_cell_at(3, 5)
+      expect(@world.count_living_neighbors(4, 4)).to eq(3)
+    end
+
+    it "returns the number of living neighbors in the same row as the given cell" do
+      @world.spawn_cell_at(1, 4)
+      @world.spawn_cell_at(1, 6)
+      expect(@world.count_living_neighbors(1, 5)).to eq(2)
+    end
+
+    it "returns the number of living neighbors in the row below the given cell" do
+      @world.spawn_cell_at(8, 7)
+      @world.spawn_cell_at(8, 8)
+      @world.spawn_cell_at(8, 9)
+      expect(@world.count_living_neighbors(7, 8)).to eq(3)
     end
   end
 end
