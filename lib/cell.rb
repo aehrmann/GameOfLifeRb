3b0706ef68f6c4e609cf6cf5592a558e45269b8e
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-06-10 09:52:49 -0500

    Add Cell#neighbor_index_pairs

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,43 @@
 class Cell
+  NEIGHBOR_OFFSETS = [[-1, -1],[-1, 0],[-1, 1],
+                      [0, -1],[0, 1],
+                      [1, -1],[1, 0],[1, 1]]
+
   attr_reader :row, :col
   def initialize(row, column)
     @row, @col = row, column
+  end
+
+  def neighbor_index_pairs(max_width)
+    all_neighbor_index_pairs(self.row, self.col).select do |(row, col)|
+      within_bounds?(row, col, max_width)
+    end
+  end
+
+  private
+
+  def within_bounds?(row, col, max_width)
+      row >= 0 && row < max_width && col >= 0 && col < max_width
+  end
+
+  def all_neighbor_index_pairs(row, col)
+    NEIGHBOR_OFFSETS.map { |offset_pair| offset_index_pair([row, col], offset_pair) }
+  end
+
+  def offset_index_pair((row, col), (row_offset, col_offset))
+    [row + row_offset, col + col_offset]
+  end
+
+  def top_neighbor_index_pairs(row, col)
+    [[row - 1, col - 1], [row - 1, col], [row - 1, col + 1]]
+  end
+
+  def side_neighbor_index_pairs(row, col)
+    [[row, col - 1], [row, col + 1]]
+  end
+
+  def bottom_neighbor_index_pairs(row, col)
+    [[row + 1, col - 1], [row + 1, col], [row + 1, col + 1]]
   end
 
 end
--- spec/lib/cell_spec.rb
+++ spec/lib/cell_spec.rb
@@ -9,15 +9,31 @@ describe Cell do
     end
   end
 
-  describe "generating neighbors index pairs" do
+  describe "generating neighbors index pairs within the bounds of the board" do
+    let(:placeholder_width) { 10 }
+
     context "when the cell is touching an edge" do
-      it "generates all legal index pairs" do
+      it "generates all legal index pairs for a cell in the left most column" do
         cell = Cell.new(4, 0)
-        expected_index_pairs = [[1, 0],[0, 1],[]]
+        expected_index_pairs = [[3, 0],[3, 1],[4, 1],[5, 0],[5, 1]]
+
+        expect(cell.neighbor_index_pairs(placeholder_width)).to eq(expected_index_pairs)
+      end
+
+      it "generates all legal index pairs for a cell in the right most column" do
+        cell = Cell.new(3, 4)
+        expected_index_pairs = [[2, 3],[2, 4],[3, 3],[4, 3],[4, 4]]
+
+        expect(cell.neighbor_index_pairs(5)).to eq(expected_index_pairs)
       end
     end
-    it "generates generates index pairs for all 8 neighbors" do
-      cell = Cell.new(3, 5)
+
+    context "when the cell is not touching any edges" do
+      it "generates generates index pairs for all 8 neighbors" do
+        cell = Cell.new(3, 5)
+        expected_index_pairs = [[2, 4],[2, 5],[2, 6],[3, 4],[3, 6],[4, 4],[4, 5],[4, 6]]
+        expect(cell.neighbor_index_pairs(placeholder_width)).to eq(expected_index_pairs)
+      end
     end
   end
 end
