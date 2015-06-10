79e59c50472b0ea9faebf664ec320f626edbc70e
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-06-10 14:27:08 -0500

    Add runner script and simple glider file

---
+++ run.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib/grid_factory'
+
+glider_contents = File.read("glider.txt")
+
+grid = GridFactory.from_parsed_input(glider_contents)
+
+running = true
+while running
+  begin
+    system "clear"
+    puts grid
+    grid = grid.tick
+    sleep 0.25
+  rescue Interrupt
+    system "clear"
+    puts "Goodbye!"
+    running = false
+  end
+end
