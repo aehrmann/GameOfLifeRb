30ed46598f2df929348d85dc9cf16cea759264bd
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-06-29 16:49:18 -0500

    Replace Location struct with Location class in Grid

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,8 +1,4 @@
-Location = Struct.new(:row, :column) do
-  def shift(row_offset, column_offset)
-    Location.new(row + row_offset, column + column_offset)
-  end
-end
+require 'location'
 
 Cell = Struct.new(:alive)
 
@@ -30,25 +26,9 @@ class Grid
 
   def number_of_living_neighbors(location)
     living_neighbors = 0
-    neighboring_locations(location).each do |locations|
-      locations.each { |location| living_neighbors += 1 if live_cell_at?(location) }
+    location.neighboring_locations.each do |location|
+      living_neighbors += 1 if live_cell_at?(location)
     end
     living_neighbors
-  end
-
-  private
-
-  def neighboring_locations(location)
-    top_neighbors = [location.shift(-1, -1),
-                     location.shift(-1, 0),
-                     location.shift(-1, 1)]
-
-    side_neighbors = [location.shift(0, -1),
-                      location.shift(0, 1)]
-
-    bottom_neighbors = [location.shift(1, -1),
-                        location.shift(1, 0),
-                        location.shift(1, 1)]
-    [top_neighbors, side_neighbors, bottom_neighbors]
   end
 end
