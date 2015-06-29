0c962c4f0b64e52204346d4f23d0f4fc8eca3d82
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-06-29 14:49:32 -0500

    Add method to check for live cell at a location
    * need to find way to differentiate between no cell and dead cell

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,9 +1,18 @@
 class Grid
+  attr_reader :locations
+  def initialize
+    @locations = {}
+  end
 
   def spawn_cell_at(row, column)
+    locations[[row, column]] = true
+  end
+
+  def kill_cell_at(row, column)
+    locations[[row, column]] = false
   end
 
   def live_cell_at?(row, column)
-    true
+    locations[[row, column]] == true
   end
 end
--- spec/lib/grid_spec.rb
+++ spec/lib/grid_spec.rb
@@ -9,4 +9,29 @@ describe Grid do
       expect(grid.live_cell_at?(0, 0)).to be true
     end
   end
+
+  describe "#live_cell_at?" do
+    let(:grid) { Grid.new }
+
+    context "when there is a live cell at a location" do
+      it "returns true" do
+        grid.spawn_cell_at(1, 1)
+        expect(grid.live_cell_at?(1, 1)).to be true
+      end
+    end
+
+    context "when there is no cell at a location" do
+      it "returns false" do
+        expect(grid.live_cell_at?(1, 1)).to be false
+      end
+    end
+
+    context "when there is a dead cell at a location" do
+      it "returns false" do
+        grid.spawn_cell_at(2, 2)
+        grid.kill_cell_at(2, 2)
+        expect(grid.live_cell_at?(2, 2)).to be false
+      end
+    end
+  end
 end
