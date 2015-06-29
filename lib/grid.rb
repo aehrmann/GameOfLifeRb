2305163d6e0ed778dd7fd4482f6347087f7003d3
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-06-29 14:41:59 -0500

    Clear out old Grid and spec code; add basic get and set cell methods
    * #spawn_cell_at creates a live cell at a location
    * #live_cell_at? checks if a cell is alive at a location

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,70 +1,9 @@
 class Grid
-  attr_reader :dimension, :cells
-
-  def cell_at(row, column)
-    cells[row][column]
-  end
 
   def spawn_cell_at(row, column)
-    cells[row][column].live!
   end
 
-  def tick
-    new_cells = cells.clone
-
-    cells_to_update.each { |cell| cell.alive? ? cell.die! : cell.live! }
-
-    Grid.new(dimension, new_cells)
-  end
-
-  def cells_to_update
-    results = []
-    self.each_cell do |cell|
-      if cell.alive?
-        results.push(cell) if overpopulated?(cell) || underpopulated?(cell)
-      else 
-        results.push(cell) if can_reproduce?(cell)
-      end
-    end
-    results
-  end
-
-  def each_cell
-    (0...dimension).each do |row|
-      (0...dimension).each do |col|
-        yield cell_at(row, col)
-      end
-    end
-  end
-
-  def all_cells_dead?
-    self.each_cell do |cell|
-      return false if cell.alive?
-    end
+  def live_cell_at?(row, column)
     true
-  end
-
-
-  def ==(other)
-    self.cells == other.cells
-  end
-
-  private
-
-  def initialize(dimension, cells)
-    @cells = cells
-    @dimension = dimension
-  end
-
-  def overpopulated?(cell)
-    cell.number_of_living_neighbors(self) > 3
-  end
-
-  def underpopulated?(cell)
-    cell.number_of_living_neighbors(self) < 2
-  end
-
-  def can_reproduce?(cell)
-    cell.number_of_living_neighbors(self) == 3
   end
 end
--- spec/lib/grid_spec.rb
+++ spec/lib/grid_spec.rb
@@ -1,71 +1,12 @@
 require 'grid'
-require 'grid_factory'
 
 describe Grid do
-
-  describe "getting a cell at a specific row and column" do
-    it "returns the cell at the given row and column" do
-      grid = GridFactory.from_string_array(["__",
-                                            "_@"])
-
-      expect(grid.cell_at(1, 1)).to eq(Cell.new(1, 1, true))
-    end
-  end
-
-  describe "finding cells that will be updated on the next tick" do
-    it "returns no cell index pairs for an empty grid" do
-      grid = GridFactory.empty_grid(10)
-      expect(grid.cells_to_update).to eq([])
-    end
-
-    it "returns live cells with fewer than 2 living neighbors" do
-      grid = GridFactory.from_string_array(["@_@",
-                                            "__@",
-                                            "___"])
-
-      expect(grid.cells_to_update).to include(grid.cell_at(0, 0))
-      expect(grid.cells_to_update).to include(grid.cell_at(0, 2))
-    end
-
-    it "returns live cells with more than 3 living neighbors" do
-      grid = GridFactory.from_string_array(["__@_",
-                                            "__@@",
-                                            "__@@",
-                                            "____"])
-      expect(grid.cells_to_update).to include(grid.cell_at(1, 3))
-    end
-
-   it "returns dead cells with exactly 3 living neighbors" do
-      grid = GridFactory.from_string_array(["_@",
-                                            "@@"])
-      expect(grid.cells_to_update).to include(grid.cell_at(0, 0))
-    end
-  end
-
-  describe "reporting if all cells are dead" do
-    it "returns true if there are no living cells" do
-      grid = GridFactory.from_string_array(["_@",
-                                            "@@"])
-      expect(grid.all_cells_dead?).to be false
-    end
-
-    it "returns true if there are no living cells" do
-      grid = GridFactory.empty_grid(10)
-      expect(grid.all_cells_dead?).to be true
-    end
-  end
-
-  describe "one tick" do
-    it "returns an updated version of the grid" do
-      grid_before = GridFactory.from_string_array([ '@_@_',
-                                                    '__@_',
-                                                    '@@__',
-                                                    '@@__'])
-      grid_after = GridFactory.from_string_array(['_@__',
-                                                  '@_@_',
-                                                  '@_@_',
-                                                  '@@__'])
-      expect(grid_before.tick).to eq(grid_after)
+  
+  describe "#spawn_cell_at" do
+    it "creates a living cell at the given row and column" do
+      grid = Grid.new
+      grid.spawn_cell_at(0, 0)
+      expect(grid.live_cell_at?(0, 0)).to be true
     end
   end
 end
