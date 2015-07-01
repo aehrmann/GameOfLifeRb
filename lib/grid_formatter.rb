0dbd35d53f00a2d965a6ad8e975ebf323938b264
Ari Ehrmann <ari.ehrmann@gmail.com>
2015-07-01 12:54:41 -0500

    Add GridFormatter.as_string

--- lib/grid_formatter.rb
+++ lib/grid_formatter.rb
@@ -1,75 +1,42 @@
 class GridFormatter
-  HORIZONTAL_PADDING = 4
-  VERTICAL_LINE_PADDING = 2
+  WIDTH = 60
+  HEIGHT = 30
 
-  attr_reader :grid, :center_width
+  TOP_OFFSET = (HEIGHT / 2) - 1
+  LEFT_OFFSET = WIDTH / 2
 
-  def initialize(grid)
-    @grid = grid
-    @center_width = (grid.dimension * 2) - 1
-    @dimension_indexes = (0...grid.dimension)
-  end
+  def self.as_string(grid)
 
-  def as_string
-    grid_header + grid_display_string + grid_footer
-  end
-
-  private
-
-  def grid_header
-    bar + empty_lines(VERTICAL_LINE_PADDING)
-  end
-
-  def grid_footer
-    empty_lines(VERTICAL_LINE_PADDING) + bar
-  end
-
-  def grid_display_string
-    @dimension_indexes.reduce("") do |string, row|
-      string << string_for_row(row)
+    character_matrix = self.new_character_matrix 
+    grid.cells.keys.each do |location|
+      if grid.live_cell_at?(location)
+        current_offset_row = location.row + TOP_OFFSET
+        current_offset_column = location.column + LEFT_OFFSET
+        if self.within_bounds?(current_offset_row, current_offset_column)
+          character_matrix[current_offset_row][current_offset_column] = '@'
+        end
+      end
     end
+
+    joined_display_string = character_matrix.map { |row| row.join('') }.join("\n")
+
+     self.bar + joined_display_string + "\n" + self.bar
   end
 
-  def string_for_cell(row, col)
-    last_column?(col) ? character_for_cell(row, col) : character_for_cell(row, col) + '-'
+  def self.bar
+    ('=' * WIDTH) + "\n"
   end
 
-  def last_column?(column)
-    column + 1 == grid.dimension
+  def self.empty_line
+    '|' + ('-' * ( WIDTH - 2 )) + '|' + "\n"
   end
 
-  def character_for_cell(row, col)
-    grid.cell_at(row, col).alive? ? '@' : '-'
+  def self.new_character_matrix
+    (self.empty_line * HEIGHT).split("\n").map { |line| line.split(//) }
   end
 
-  def string_for_row(row)
-    row_left + string_for_grid_row(row) + row_right + "\n"
-  end
-
-  def string_for_grid_row(row)
-    @dimension_indexes.reduce("") do |string, col|
-      string << string_for_cell(row, col)
-    end
-  end
-
-  def row_right
-    ('-' * HORIZONTAL_PADDING) + '|'
-  end
-
-  def row_left
-    '|' + ('-' * HORIZONTAL_PADDING) 
-  end
-
-  def empty_lines(count)
-    empty_line * count
-  end
-
-  def empty_line
-    '|' + ('-' * HORIZONTAL_PADDING) + ('-' * center_width) + ('-' * HORIZONTAL_PADDING) + '|' + "\n"
-  end
-
-  def bar
-    '=' * ((2 * grid.dimension) + (HORIZONTAL_PADDING * 2) + 1) + "\n"
+  def self.within_bounds?(row, column)
+    row < HEIGHT && column < WIDTH
   end
 
 end
